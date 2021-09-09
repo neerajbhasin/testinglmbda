8c6e83f4a48bab6391dff838577d317e87134df5
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-09 16:58:48 -0400

    aws region in yaml file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello neeraj, from Lambda!"),
+    body: JSON.stringify(
+      "This is my first time playing with lambda and ci/cd!"
+    ),
   };
   return response;
 };
