6955711eb612bc8d574b80d535dbb37c9afc0c41
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-10 11:53:01 -0400

    testign with another dir5

--- testing2/index.js
+++ testing2/index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
   // TODO implement
+  console.log("sd");
   const response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda! this is testing2 function!!a!!!"),
