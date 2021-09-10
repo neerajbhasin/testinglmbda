2bd828ffc3f6fe40ff691b3359c7a633784e5597
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-10 15:40:10 -0400

    as123456789

--- testing2/index.js
+++ testing2/index.js
@@ -2,11 +2,11 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(10, 100);
-  console.log("sddsfaassdsd");
+  console.log("sddsfaasssdsd");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
-      `Hello from Lambda! ${num}this is testing2 function!!a!!!`
+      `Hello from Lambda! ${num}this is testing2 function!!ab!!!`
     ),
   };
   return response;
