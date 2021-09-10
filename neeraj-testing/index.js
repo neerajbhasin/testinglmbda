a289fc62928cef95449167c0b5c43109b2e7784b
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-10 10:33:47 -0400

    change in index file

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,6 +3,7 @@ import _ from "lodash";
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
+  console.log("num");
   const response = {
     statusCode: 200,
     body: JSON.stringify(`Generating random number in lambda ${num}`),
