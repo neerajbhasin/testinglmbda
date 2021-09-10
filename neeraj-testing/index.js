915c2c4915bf18419c2a892c05b0d502e2821c18
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-10 17:05:45 -0400

    sfsasdasaa

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify(
-      `9Generating random number in lambda${num}. I am gonna make this thing work!!!!`
+      `10Generating random number in lambda${num}. I am gonna make this thing work!!!!`
     ),
   };
   return response;
