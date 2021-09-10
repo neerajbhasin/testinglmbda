9f718bfa0c7844a19ed22351507d74c045b1bb93
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-10 11:40:14 -0400

    testign with another dir1

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("num5");
+  console.log("num6");
   const response = {
     statusCode: 200,
     body: JSON.stringify(`Generating random number in lambda ${num}`),
