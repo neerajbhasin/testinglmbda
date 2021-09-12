59000982f8f8b0cd7861b785f7fb7229062f1c01
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 14:57:43 -0400

    neeraj/testing/index.js

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("numassssssssdassaassdaadasssssaszdss7");
+  console.log("updating this fiel");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
