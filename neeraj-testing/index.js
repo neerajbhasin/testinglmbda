358cb26d3ff1845b8013f569c66e8ed48712b5bd
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 15:03:47 -0400

    checking path of files change2

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("checking updatea this file");
+  console.log("checking updateaa this file");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
