1810d73f0ecfe307a2506c5629855eb541602d0a
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 15:28:24 -0400

    chang git diffs

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("checking dasdsjssdsxczfsfsdhsdsfadfsdsad this file");
+  console.log("checking dasdsjasdssdsxczfsfsdhsdsfadfsdsad this file");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
