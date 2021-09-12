b37c2c5742365e1dc4308285e5ae9083ec83a930
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 16:18:54 -0400

    checking if condition for job

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("12abcsdsSsdadsfaazsad");
+  console.log("12abcsdsSsdasdsfaazsad");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
