2e5352bfd804a577de0c5bdc954fe20a98488217
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 19:35:50 -0400

    modifiedayesaa

--- testing2/index.js
+++ testing2/index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(10, 100);
-  console.log("abcsssda");
+  console.log("abcsssdfssda");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
