0483eadcc3fba19544f5b3249abd2866b54acb70
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 15:20:43 -0400

    jkhf

--- neeraj-testing/index.js
+++ neeraj-testing/index.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(100, 999);
-  console.log("checking djssdfdhsdadfsdsad this file");
+  console.log("checking djssdfsfsdhsdsfadfsdsad this file");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
