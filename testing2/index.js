a30d1079bbae6c098fd53ecafbdce1c9b23c07a9
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 15:25:28 -0400

    chang git diff

--- testing2/index.js
+++ testing2/index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(10, 100);
-  console.log("sddsfaassdfkhadadsdasdassfdssssdsd");
+  console.log("sddsfaassdfkhasdadsdasdassfdssssdsd");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
