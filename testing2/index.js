001c11526243a2d8e4d3be101177f50e235332c9
neerajbhasin <neeraj.bhasin@fintelics.com>
2021-09-12 15:10:32 -0400

    checking path of files change4

--- testing2/index.js
+++ testing2/index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 exports.handler = async (event) => {
   // TODO implement
   const num = _.random(10, 100);
-  console.log("sddsfaasdadsdssssdsd");
+  console.log("sddsfaasdadsdasdasssssdsd");
   const response = {
     statusCode: 200,
     body: JSON.stringify(
